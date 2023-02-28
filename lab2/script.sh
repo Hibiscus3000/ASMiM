e05adddd209a2e9d8048335626feb0ddacb6dafa
Valeriy Sinyukov <v.sinyukov@g.nsu.ru>
2023-02-28 16:03:18 +0700

    lab2: script

--- lab2/script.sh
+++ lab2/script.sh
@@ -1,21 +1,52 @@
 #!/bin/bash
 
 assembly_file=main_assembly.s
+csv_file=table.csv
 executable_file=./main_assembly
+n=$((10240 * (128 + 512 + 3072) / 4))
+attempts=$((2))
+
+init=$((0))
+finish=$((300))
 
 >$assembly_file
+>$csv_file
 
-for ((i = 230; i < 260; i++))
-do
-    cat start.s >> $assembly_file
-    for ((k = 0; k < i; k++))
+prepare_assembly() {
+    echo "$2" > $assembly_file
+    for ((k = 0; k < $1; k ++))
     do
-        printf "\nnop" >> $assembly_file
+        printf "\tnop\n" >> $assembly_file
     done
-    printf "\n" >> $assembly_file
-    cat finish.s >> $assembly_file
-    printf "$i nops "
-    gcc $assembly_file -o $executable_file
-    $executable_file
-    > $assembly_file
+    echo "$3" >> $assembly_file
+    if [[ $5 = 1 ]]
+    then
+        for ((k = 0; k < $1; k ++))
+        do
+            printf "\tnop\n" >> $assembly_file
+        done
+    fi
+    echo "$4" >> $assembly_file
+}
+
+test() {
+    for  ((j = 0; j < $attempts; j++))
+    do
+        printf "\n" >> $csv_file
+        for ((i = $init; i < $finish; i++))
+        do
+            prepare_assembly $i "$2" "$3" "$4" $5
+            gcc $assembly_file -o $executable_file
+            printf "($1,$j,$i): "
+            $executable_file $n $csv_file
+        done
+    done
+}
+
+for ((i = $init; i < $finish; i++))
+do
+    printf "$i;" >> $csv_file
 done
+test 2 "$(cat start_reg.s)" "" "$(cat finish_reg.s)" 0
+printf "\n" >> $csv_file
+test 2 "$(cat start2_reg.s)" "$(cat middle2_reg.s)" "$(cat finish2_reg.s)" 1
